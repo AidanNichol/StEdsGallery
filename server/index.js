8620ea285ceff0e9a12bf4e49298a981d789344f
Aidan Nichol <aidan@nicholware.co.uk>
2021-05-21 12:21:48 +0100

    ignore logs

--- server/index.js
+++ server/index.js
@@ -1,56 +1,56 @@
 // Require the framework and instantiate it
-const fastifyPkg = require('fastify');
-const fastifyCors = require('fastify-cors');
-const fastifyCookie = require('fastify-cookie');
-const fastifyStatic = require('fastify-static');
-const multipart = require('fastify-multipart');
+const fastifyPkg = require("fastify");
+const fastifyCors = require("fastify-cors");
+const fastifyCookie = require("fastify-cookie");
+const fastifyStatic = require("fastify-static");
+const multipart = require("fastify-multipart");
 
-const cpgRoutes = require('./cpgRoutes.js');
-const { authRoutes } = require('./authRoutes.js');
+const cpgRoutes = require("./cpgRoutes.js");
+const { authRoutes } = require("./authRoutes.js");
 
-const getEnv = require('getenv');
-const path = require('path');
-const jetpack = require('fs-jetpack');
-const getenv = require('getenv');
+const getEnv = require("getenv");
+const path = require("path");
+const jetpack = require("fs-jetpack");
+const getenv = require("getenv");
 const { cwd, read } = jetpack;
 const galleryDataPath = process.env.GALLERY_DATA;
-console.log('galleryData', galleryDataPath);
-const fs = require('fs');
+console.log("galleryData", galleryDataPath);
+const fs = require("fs");
 
-const https = getenv.bool('DEVELOPMENT')
+const https = getenv.bool("DEVELOPMENT")
   ? {
       https: {
-        key: read('./server.key'),
-        cert: read('./server.crt'),
+        key: read("./server.key"),
+        cert: read("./server.crt"),
       },
     }
   : {};
 const fastify = fastifyPkg({
   // ...https,
   logger: {
-    level: 'info',
-    file: 'logs/fastify.txt', // will use pino.destination()
+    level: "info",
+    file: "logs/fastify.log", // will use pino.destination()
   },
 });
 fastify.register(fastifyCookie, {
-  secret: getenv('COOKIE_SECRET'), // for cookies signature
+  secret: getenv("COOKIE_SECRET"), // for cookies signature
   parseOptions: {}, // options for parsing cookies
 });
 fastify.register(multipart, { attachFieldsToBody: true });
 
 fastify.register(fastifyStatic, {
   root: galleryDataPath,
-  prefix: '/galleryData', // optional: default '/'
+  prefix: "/galleryData", // optional: default '/'
 });
 // fastify.register(fastifyCors, {
 //   credentials: true,
 //   origin: [/localhost/, /stedwardsfellwalkers\.co\.uk$/],
 // });
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' };
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
 });
-fastify.register(cpgRoutes, { prefix: 'cpg' });
-fastify.register(authRoutes, { prefix: 'auth' });
+fastify.register(cpgRoutes, { prefix: "cpg" });
+fastify.register(authRoutes, { prefix: "auth" });
 
 // Run the server!
 const runit = async () => {
@@ -60,6 +60,6 @@ const runit = async () => {
     fastify.log.error(err);
     process.exit(1);
   }
-  console.log('listening on localhost:5555');
+  console.log("listening on localhost:5555");
 };
 runit();
