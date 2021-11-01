bb5bc1847dcefb836a57c70e36226de0645c3a08
Aidan Nichol <aidan@nicholware.co.uk>
2021-11-01 14:25:31 +0000

    use jetpack find rather than inspect to simplify code

--- setup/convertFeatures.js
+++ setup/convertFeatures.js
@@ -3,52 +3,37 @@ let { extractFeatures } = require('../server/mappdf/extractFeatures')
 const _ = require("lodash");
 const features = [];
 
-function walkTree(branch) {
+console.log("converting", tFile);
+let jFile = tFile.replace(/\.txt$/, ".json");
+// if (jetpack.exists(`${subdir}/${jFile}`)) continue;
+let data = jetpack.read(tFile, "utf8");
+data = data
+  .replace(/ => /g, ":")
+  .replace(/\n/g, "")
+let wData = data.match(/[$]wData=array \(([^;]*)\);/);
+wData = (wData || [, ''])[1]
+let wp = data.match(/[$]wp=array \(([^;]*)\);/);
+wp = (wp || [, ''])[1];
 
-  // console.log(branch.relativePath);
-  for (const twig of branch.children) {
-    if (twig.type === "dir") walkTree(twig);
-    if (twig.type === "file") {
-      convertToJson(twig);
-    }
-  }
-}
-function convertToJson(file) {
-  if (!/^feature/i.test(file.name)) return;
-  if (!/^feature.*\.txt$/i.test(file.name)) return;
-  // let wp;
-  let tFile = file.relativePath;
-  console.log("converting", tFile);
-  let jFile = tFile.replace(/\.txt$/, ".json");
-  // if (jetpack.exists(`${subdir}/${jFile}`)) continue;
-  let data = jetpack.read(tFile, "utf8");
-  data = data
-    .replace(/ => /g, ":")
-    .replace(/\n/g, "")
-  let wData = data.match(/[$]wData=array \(([^;]*)\);/);
-  wData = (wData || [, ''])[1]
-  let wp = data.match(/[$]wp=array \(([^;]*)\);/);
-  wp = (wp || [, ''])[1];
+wp = wp
 
-  wp = wp
+  .replace(/array \(/g, "{")
+  .replace(/\)/g, "}")
+  .replace(/\d+\s*:/g, '');
 
-    .replace(/array \(/g, "{")
-    .replace(/\)/g, "}")
-    .replace(/\d+\s*:/g, '');
-
-  let obj = Function(`"use strict";return ({wData:{${wData}}, wp:[${wp}]})`)();
-  obj.wp = obj.wp.map((wp) => _.pick(wp, ["name", "pos", "dist", "bear"]));
-  let features = extractFeatures(obj);
-  jetpack.write(jFile, features);
+let obj = Function(`"use strict";return ({wData:{${wData}}, wp:[${wp}]})`)();
+obj.wp = obj.wp.map((wp) => _.pick(wp, ["name", "pos", "dist", "bear"]));
+let features = extractFeatures(obj);
+jetpack.write(jFile, features);
 }
 console.log("started");
 try {
   jetpack = jetpack.cwd("/Users/aidan/Websites/htdocsC/walkdata");
   console.log("cwd", jetpack.cwd());
-  let tree = jetpack.inspectTree(".", {
-    relativePath: true,
-  });
-  walkTree(tree);
+  let featFiles = jetpack.find('.', { matching: './**/features*.txt' })
+  for (let file of featFiles) {
+    convertToJson(file);
+  }
 } catch (error) {
   console.log(error);
 }
