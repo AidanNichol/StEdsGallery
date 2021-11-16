69e28cac5604bd6ec0ec6e3e79b1e0196f0d19e4
Aidan Nichol <aidan@nicholware.co.uk>
2021-11-16 11:51:25 +0000

    support styling of place names

--- server/mappdf/decorateRoutes.js
+++ server/mappdf/decorateRoutes.js
@@ -1,7 +1,16 @@
 const blackDot = (doc, m, x, y) => doc.setFillColor(0).circle(m.px(x), m.py(y), 0.5, 'F');
+const redDot = (doc, m, x, y, dx=0,dy=0) => doc.setFillColor(255,0,0).circle(m.px(x,dx), m.py(y,dy), 0.5, 'F');
 function decorateRoutes(doc, m) {
+  
+  for (const pt of m.segPoints) {
+    const s = 2;
+    
+    doc
+    .setFillColor(180)
+    .rect(m.px(pt.x) - 0.5 * s, m.py(pt.y) - 0.5 * s, s, s, 'F')
+    .stroke();
+  }
   doc.setDrawColor(128, 0, 128);
-
   for (const pt of m.starts) {
     doc.setFillColor(255, 255, 0).circle(m.px(pt.x), m.py(pt.y), -1.5, 'DF').stroke();
     blackDot(doc, m, pt.x, pt.y);
@@ -13,35 +22,47 @@ function decorateRoutes(doc, m) {
       .stroke();
     blackDot(doc, m, pt.x, pt.y);
   }
-  for (const pt of m.segPoints) {
-    const s = 2;
-    doc
-      .setFillColor(180)
-      .rect(m.px(pt.x) - 0.5 * s, m.py(pt.y) - 0.5 * s, s, s, 'F')
-      .stroke();
-  }
-  // Draw Names
 }
+// Draw Names
+// ! Note we don't use the align option on the doc.text method becuase
+// ! when it has a value of other than left the angle option doesn't work properly
+// ! in stead we adjust the x value to pull the start of text to the right
+// ! so that so far as jsPdf is concerned the text is always left aligned from
+// ! the x,y value and angle just works.
 function drawNames(doc, m) {
   doc.setFont('helvetica', 'normal').setFontSize(8).setTextColor(20);
   // doc.setFillColor(0);
   for (const name in m.names) {
+    let ftHt= 8/72*25.6;
+    let size = doc.getStringUnitWidth(` ${name} `)*ftHt;
     const pt = m.names[name];
-    let dx = 0;
-    let opts = { align: 'left', baseline: 'middle' };
-    if (/R/.test(pt.shift)) opts.align = 'right';
-    if (/C/.test(pt.shift)) opts.align = 'center';
-    if (/T/.test(pt.shift)) opts.baseline = 'top';
-    if (/B/.test(pt.shift)) opts.baseline = 'bottom';
+    let angle=0;
     if (/A/.test(pt.shift)) {
-      let match = pt.shift.match(/A(\d+)/)
-      opts.angle = parseInt(match[1])
+      let match = pt.shift.match(/A([-\d]+)/)
+      angle = parseInt(match[1])
     }
-    if (pt.start && opts.align === 'left') dx = 1.3;
-    if (pt.end && opts.align === 'right') dx = -1.3;
-    console.log(name, pt.shift, opts, dx);
+    // let angle = (rad * 180) / Math.PI;
+    let rad = angle*Math.PI/180;
+    let opts = { align: 'left', angle};
+    let opp=Math.sin(rad)*size;
+    let adj=Math.cos(rad)*size;
+    let dx = 0, dy=ftHt*0.3;
+    if (/R/.test(pt.shift)) dx=-adj;
+    if (/C/.test(pt.shift)) dx=-adj/2;
+    if (/T/.test(pt.shift)) dy=ftHt*0.9;
+    if (/B/.test(pt.shift)) dy=-ftHt*0.1;
+    dy+=opp;
+    if (pt.start||pt.end){
+      if (  /R/.test(pt.shift)) dx -= ftHt/2;
+      else if ( ! /C/.test(pt.shift)) dx += ftHt/2;
+    }
+    if (/X/.test(pt.shift)) dx+=parseInt(pt.shift.match(/X([-\d]+)/)[1])
+    if (/Y/.test(pt.shift)) dy+=parseInt(pt.shift.match(/Y([-\d]+)/)[1])
+    // console.log(name, pt.shift, opts, pt, dx, dy);
+      // doc.setFillColor(200).rect(m.px(pt.x, dx), m.py(pt.y, -ftHt+dy), adj, ftHt, 'F')
     blackDot(doc, m, pt.x, pt.y);
-    doc.text(` ${name} `, m.px(pt.x) + dx, m.py(pt.y), opts);
+    doc.text(` ${name} `, m.px(pt.x, dx), m.py(pt.y,dy), opts);
+    // redDot(doc, m, pt.x,pt.y, dx,dy);
   }
 }
 module.exports = { decorateRoutes, drawNames };
