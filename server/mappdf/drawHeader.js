7d1b2e882d1ad423316c7413d48b8d7e4b4fe619
Aidan Nichol <aidan@nicholware.co.uk>
2021-12-01 11:41:13 +0000

    fix missing header on side in portrait mode

--- server/mappdf/drawHeader.js
+++ server/mappdf/drawHeader.js
@@ -20,16 +20,23 @@ function drawHeader(doc, m) {
     doc.setFontSize(8).text(m.regName, 19, 12);
     doc
       .setFontSize(8)
-      .text(format(new Date(m.walk), "EEEE, Do MMM, yyyy"), 19, 15);
+      .text(format(new Date(m.walk), "EEEE, do MMM, yyyy"), 19, 15);
     const createdDate = format(new Date(), " d MMM yyyy, HH:mm:ss");
-    let startD = 170;
-    if (m.orientation === "portrait") startD = m.header === "T" ? 170 : 257;
-    else startD = m.header === "T" ? 257 : 170;
-    doc.text(createdDate, startD - m.margin, m.margin);
+    let headerWidth = 210;
+    let sideHeaderStart = 297;
+    if (m.orientation === "P") {
+      headerWidth = m.header === "T" ? 210 : 297;
+      sideHeaderStart = 210;
+    } else headerWidth = m.header === "T" ? 297 : 210;
+    doc.text(createdDate, headerWidth - 40 - m.margin, m.margin);
     doc.endFormObject("head");
     const mrg = m.margin;
-    const sideMatrix = new doc.Matrix(0, 1, -1, 0, 297 - mrg, mrg);
+    const sideMatrix = new doc.Matrix(0, 1, -1, 0, sideHeaderStart - mrg, mrg);
     const topMatrix = new doc.Matrix(1, 0, 0, 1, mrg, mrg);
+    const matrix =
+      m.header === "T"
+        ? new doc.Matrix(1, 0, 0, 1, mrg, mrg)
+        : new doc.Matrix(0, 1, -1, 0, sideHeaderStart - mrg, mrg);
 
     doc.doFormObject("head", m.header === "T" ? topMatrix : sideMatrix);
   });
