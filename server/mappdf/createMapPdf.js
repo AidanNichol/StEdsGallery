f41a2800b5e3deeba76bcd4c64d12c490c1fe80a
Aidan Nichol <aidan@nicholware.co.uk>
2023-02-08 11:20:42 +0000

    added error capture

--- server/mappdf/createMapPdf.js
+++ server/mappdf/createMapPdf.js
@@ -15,54 +15,58 @@ const walkdata = "/Users/aidan/Websites/htdocsC";
 // let ver = 100;
 
 async function createMapPdf(walkNo, walkData) {
-  console.log("createMap", walkNo);
-  const map = await extractMapData(walkNo, walkData);
+	try {
+		console.log("createMap", walkNo);
+		const map = await extractMapData(walkNo, walkData);
 
-  map.minY -= walkData.bottom;
-  map.maxY += walkData.top;
-  map.minX -= walkData.left;
-  map.maxX += walkData.right;
-  GetPageLayout(map);
-  const doc = new jsPDF({ orientation: map.orientation });
-  // console.log(doc.getFontList());
+		map.minY -= walkData.bottom;
+		map.maxY += walkData.top;
+		map.minX -= walkData.left;
+		map.maxX += walkData.right;
+		GetPageLayout(map);
+		const doc = new jsPDF({ orientation: map.orientation });
+		// console.log(doc.getFontList());
 
-  drawFeatures(doc, map);
-  drawGrid(doc, map);
-  decorateRoutes(doc, map);
-  drawSegments(doc, map);
-  drawNames(doc, map);
-  drawHeader(doc, map);
-  drawKey(doc, map);
-  let pdf = `walkdata/${walkNo.substr(0, 4)}/${walkNo}/map-${walkNo}.pdf`;
-  doc.save(`${walkdata}/${pdf}`); // will save the file in the current working directory
-  await remodelProfiles(walkNo, map.routes);
-  // pdf = `${pdf}?v=${++ver}}`;
-  return [pdf, map.orientation, map];
-  // return { img: pdf, map };
+		drawFeatures(doc, map);
+		drawGrid(doc, map);
+		decorateRoutes(doc, map);
+		drawSegments(doc, map);
+		drawNames(doc, map);
+		drawHeader(doc, map);
+		drawKey(doc, map);
+		let pdf = `walkdata/${walkNo.substr(0, 4)}/${walkNo}/map-${walkNo}.pdf`;
+		doc.save(`${walkdata}/${pdf}`); // will save the file in the current working directory
+		await remodelProfiles(walkNo, map.routes);
+		// pdf = `${pdf}?v=${++ver}}`;
+		return [pdf, map.orientation, map];
+		// return { img: pdf, map };
+	} catch (error) {
+		console.error(error);
+	}
 }
 function GetPageLayout(map) {
-  const headSz = 20;
-  ["P", "L"].forEach((O) => {
-    wdth = O === "P" ? 210 : 297;
-    hght = O === "P" ? 297 : 210;
-    width = wdth - 2 * map.margin;
-    height = hght - 2 * map.margin;
-    ["T", "S"].forEach((P) => {
-      dW = P === "T" ? 0 : headSz; // headSz is the height of the header
-      dH = P === "T" ? headSz : 0;
-      scl = Math.min((width - dW) / map.rangeX, (height - dH) / map.rangeY);
-      if (scl > map.scale) {
-        map.scale = scl;
-        map.orientation = O;
-        map.header = P;
-        map.topZero = map.margin + dH;
-        map.leftZero = map.margin;
-        map.width = wdth;
-        map.height = hght;
-      }
-    });
-  });
+	const headSz = 20;
+	["P", "L"].forEach((O) => {
+		wdth = O === "P" ? 210 : 297;
+		hght = O === "P" ? 297 : 210;
+		width = wdth - 2 * map.margin;
+		height = hght - 2 * map.margin;
+		["T", "S"].forEach((P) => {
+			dW = P === "T" ? 0 : headSz; // headSz is the height of the header
+			dH = P === "T" ? headSz : 0;
+			scl = Math.min((width - dW) / map.rangeX, (height - dH) / map.rangeY);
+			if (scl > map.scale) {
+				map.scale = scl;
+				map.orientation = O;
+				map.header = P;
+				map.topZero = map.margin + dH;
+				map.leftZero = map.margin;
+				map.width = wdth;
+				map.height = hght;
+			}
+		});
+	});
 
-  return;
+	return;
 }
 module.exports = { createMapPdf };
